9594a2b325e24a104d9d5c0ea500f54418c44437
Gonzalo Silva <30927470+gonzalosil@users.noreply.github.com>
2023-07-14 18:02:44 -0300

    hice lo de navlink

--- preentrega2-silva/src/components/NavBar/NavBar.js
+++ preentrega2-silva/src/components/NavBar/NavBar.js
@@ -1,58 +1,28 @@
 // import Button from 'react-bootstrap/Button';
-import Container from 'react-bootstrap/Container';
 // import Form from 'react-bootstrap/Form';
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
-// import NavDropdown from 'react-bootstrap/NavDropdown';
-import Offcanvas from 'react-bootstrap/Offcanvas';
 import CartWidget from '../CartWidget/CartWidget'
+import {Link, NavLink} from 'react-router-dom';
 
 function NavBar() {
   return (
-    <>
-      {['md'].map((expand) => (
-        <Navbar key={expand} expand={expand} className="bg-body-tertiary mb-3">
-          <Container fluid>
-            <Navbar.Brand href="#">
-              <img alt="" src={'/images/RFlogo.png'} width="60" height="60" className="d-inline-block align-top"/>{' '}</Navbar.Brand>
-            <Navbar.Toggle aria-controls={`offcanvasNavbar-expand-${expand}`} />
-            <Navbar.Offcanvas
-              id={`offcanvasNavbar-expand-${expand}`}
-              aria-labelledby={`offcanvasNavbarLabel-expand-${expand}`}
-              placement="end"
-            >
-              <Offcanvas.Header closeButton>
-                <Offcanvas.Title id={`offcanvasNavbarLabel-expand-${expand}`}>
-                <img
-              alt=""
-              src={'RFlogo.png'}
-              width="60"
-              height="60"
-              className="d-inline-block align-top"
-            />{' '}
-                </Offcanvas.Title>
-              </Offcanvas.Header>
-              <Offcanvas.Body>
-                <Nav className="flex-grow-1 pe-3">
-                  {/* <Nav.Link href="#inicio" style={{ fontSize: '1.15em' }}>Inicio</Nav.Link> */}
-                  <Nav.Link href="#espejos" style={{ fontSize: '1.15em' }}>Espejos</Nav.Link>
-                  <Nav.Link href="#escritorios" style={{ fontSize: '1.15em' }}>Escritorios</Nav.Link>
-                  {/* <NavDropdown.Divider /> */}
-                  <Nav.Link href="#estanterias" style={{ fontSize: '1.15em' }}>Estanterias</Nav.Link>
-                  <Nav.Link href="#percheros" style={{ fontSize: '1.15em' }}>Percheros</Nav.Link>
-                  <Nav.Link href="#contacto" style={{ fontSize: '1.15em' }}>Contacto</Nav.Link>
-                </Nav>
-                <Nav className="justify-content-end flex-grow-1 pe-3">
-                  <Nav.Link href="#cartwidget" style={{ fontSize: '1.15em' }}><CartWidget/></Nav.Link>
-                </Nav>
-
-              </Offcanvas.Body>
-            </Navbar.Offcanvas>
-          </Container>
-        </Navbar>
-      ))}
-    </>
-  );
+    <Navbar className="bg-body-tertiary mb-3">
+      <NavLink to='/'>
+        <img alt="" src={'/images/RFlogo.png'} width="60" height="60" className="d-inline-block align-top"/>{' '}
+      </NavLink>
+      <Nav className="">
+        <Link to='/espejos' style={{ fontSize: '1.15em' }}>Espejos</Link>
+        <NavLink to='/escritorios' style={{ fontSize: '1.15em' }}>Escritorios</NavLink>
+        <NavLink to='/estanterias' style={{ fontSize: '1.15em' }}>Estanterias</NavLink>
+        <NavLink to='/percheros' style={{ fontSize: '1.15em' }}>Percheros</NavLink>
+        <NavLink to='/contacto' style={{ fontSize: '1.15em' }}>Contacto</NavLink>
+      </Nav>
+      <Nav className="justify-content-end flex-grow-1 pe-3">
+        <NavLink to='/cartwidget'><CartWidget/></NavLink>
+      </Nav>
+    </Navbar>
+  )
 }
 
 export default NavBar;
